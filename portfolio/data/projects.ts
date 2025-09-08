4095d69f6c36ff780a968f367780db2cbf3b8e31
David <davidesparzaj22@gmail.com>
2025-09-08 17:13:44 -0500

    refactor(portfolio/data/projects.ts):
    import AboutInfo from shared types file and remove
    
    inline interface definition

--- portfolio/data/projects.ts
+++ portfolio/data/projects.ts
@@ -1,16 +1,5 @@
+import { AboutInfo } from "@/types/types";
 const API_BASE_URL = process.env.NEXT_PUBLIC_DB_CONNECTION || "";
-
-export interface AboutInfo {
-  headline: string;
-  bio: string;
-  avatarIconUrl: string;
-  skills: string[];
-  techStack: string[];
-  experience?: { title: string; company: string; duration: string }[];
-  education: string[];
-  certifications?: { title: string; issuer: string; date: string }[];
-  socialLinks: string[];
-}
 
 export default async function getAboutMeInfo(): Promise<AboutInfo[]> {
   const aboutMe = await fetch(`${API_BASE_URL}/about`, {
