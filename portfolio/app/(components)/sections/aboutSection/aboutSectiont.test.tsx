5f30c98e9ca80154dad4a0d107b06dff2c57cc44
David <davidesparzaj22@gmail.com>
2025-09-09 18:41:03 -0500

    test(portfolio/app/(components)
    /sections/aboutSection/aboutSectiont.test.tsx):
    
    remove debug log and update text matcher

--- portfolio/app/(components)/sections/aboutSection/aboutSectiont.test.tsx
+++ portfolio/app/(components)/sections/aboutSection/aboutSectiont.test.tsx
@@ -3,8 +3,7 @@ import AboutSection from "./aboutSection";
 
 test("Render about section", async () => {
   const ui = await AboutSection();
-  console.log(ui);
   render(ui);
 
-  expect(screen.getByText(/About Me/i)).toBeDefined();
+  expect(screen.getByText(/About-me/i)).toBeDefined();
 });
