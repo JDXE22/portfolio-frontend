214e29a2ce0b191e1908f1e9dcaef19fce0684e1
David <davidesparzaj22@gmail.com>
2025-09-08 22:19:33 -0500

    test(portfolio/app/(components)/
    sections/aboutSection/aboutSectiont.test.tsx):
    
    update test to support async component rendering
    
    and ensure content presence

--- portfolio/app/(components)/sections/aboutSection/aboutSectiont.test.tsx
+++ portfolio/app/(components)/sections/aboutSection/aboutSectiont.test.tsx
@@ -1,8 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import AboutSection from "./aboutSection";
 
-test("Render about section", () => {
-  render(<AboutSection />);
-  const element = screen.getByText(/About Me/i);
-  expect(element).toBeDefined();
+test("Render about section", async () => {
+  const ui = await AboutSection();
+  console.log(ui);
+  render(ui);
+
+  expect(screen.getByText(/About Me/i)).toBeDefined();
 });
