f8013a7e50a969ba0d975c47a9fccfb1b05131e9
David <davidesparzaj22@gmail.com>
2025-09-08 22:05:54 -0500

    refactor(portfolio/app/(components)
    /sections/aboutSection/aboutSection.tsx):
    
    use async/await instead of promise chaining
    
    for data fetching

--- portfolio/app/(components)/sections/aboutSection/aboutSection.tsx
+++ portfolio/app/(components)/sections/aboutSection/aboutSection.tsx
@@ -2,19 +2,22 @@ import { Section } from "@/app/(components)/layout/Section";
 import getAboutMeInfo from "@/data/projects";
 
 export default async function AboutSection() {
+  const aboutInfo = await getAboutMeInfo();
+
   return (
     <Section id="about" title="About-me" subtitle="short-introduction">
       <div className="grid gap-6 sm:grid-cols-2 md: grid-cols-3">
-        {getAboutMeInfo().then((aboutInfo) => {
-          return aboutInfo.map((info) => (
-            <div className="rounded-lg border border-gray-200 bg-white p-6 shadow-md">
-              <h3 className="mb-2 text-xl font-semibold text-gray-800">
-                {info.headline}
-              </h3>
-              <p className="text-gray-600">{info.bio}</p>
-            </div>
-          ));
-        })}
+        {aboutInfo.map((info) => (
+          <div
+            key={info.headline}
+            className="rounded-lg border border-gray-200 bg-white p-6 shadow-md"
+          >
+            <h3 className="mb-2 text-xl font-semibold text-gray-800">
+              {info.headline}
+            </h3>
+            <p className="text-gray-600">{info.bio}</p>
+          </div>
+        ))}
       </div>
     </Section>
   );
