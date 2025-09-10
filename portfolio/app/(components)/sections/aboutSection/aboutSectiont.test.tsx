99172f1a69bcb9188bf9d086ab587d479b98640e
David <davidesparzaj22@gmail.com>
2025-09-09 19:50:09 -0500

    test(portfolio/app/(components)/
    sections/aboutSection/aboutSectiont.test.tsx):
    
    extend tests to cover bio and skills rendering

--- portfolio/app/(components)/sections/aboutSection/aboutSectiont.test.tsx
+++ portfolio/app/(components)/sections/aboutSection/aboutSectiont.test.tsx
@@ -3,7 +3,14 @@ import AboutSection from "./aboutSection";
 
 test("Render about section", async () => {
   const ui = await AboutSection();
+  console.log(ui);
   render(ui);
 
-  expect(screen.getByText(/About-me/i)).toBeDefined();
+  expect(screen.getByText(/About Me/i)).toBeDefined();
+  expect(
+    screen.getByText(
+      /I’m a Full‑Stack developer specializing in backend systems/i
+    )
+  ).toBeDefined();
+  expect(screen.getAllByTestId("skills-id").length).toBe(5);
 });
