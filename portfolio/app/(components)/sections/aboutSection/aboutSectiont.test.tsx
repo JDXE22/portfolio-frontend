60836990c500a875ca272d4a2af65d41e4d5655d
David <davidesparzaj22@gmail.com>
2025-09-09 20:00:55 -0500

    test(portfolio/app/(components)
    /sections/aboutSection/aboutSectiont.test.tsx):
    
    update skills count expectation to 5

--- portfolio/app/(components)/sections/aboutSection/aboutSectiont.test.tsx
+++ portfolio/app/(components)/sections/aboutSection/aboutSectiont.test.tsx
@@ -3,7 +3,6 @@ import AboutSection from "./aboutSection";
 
 test("Render about section", async () => {
   const ui = await AboutSection();
-  console.log(ui);
   render(ui);
 
   expect(screen.getByText(/About Me/i)).toBeDefined();
@@ -12,5 +11,5 @@ test("Render about section", async () => {
       /I’m a Full‑Stack developer specializing in backend systems/i
     )
   ).toBeDefined();
-  expect(screen.getAllByTestId("skills-id").length).toBe(1);
+  expect(screen.getAllByTestId("skills-id").length).toBe(5);
 });
