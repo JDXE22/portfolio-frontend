226ece60a772dc0bb5c1e4cd8bf8e7b9940ae59e
David <davidesparzaj22@gmail.com>
2025-09-10 16:58:47 -0500

    feat(datportfolio/data/projects.tsa):
    add getProjects function to fetch project data

--- portfolio/data/projects.ts
+++ portfolio/data/projects.ts
@@ -1,11 +1,18 @@
-import { AboutInfo } from "@/types/types";
+import { AboutInfo, IProject } from "@/types/types";
 import { serverEnv } from "@/config/env";
 
 const API_BASE_URL = serverEnv.PUBLIC_DB_CONNECTION;
 
-export default async function getAboutMeInfo(): Promise<AboutInfo[]> {
+export async function getAboutMeInfo(): Promise<AboutInfo[]> {
   const aboutMe = await fetch(`${API_BASE_URL}/about`, {
     cache: "no-store",
   });
   return aboutMe.json();
+}
+
+export async function getProjects(): Promise<IProject[]> {
+  const projects = await fetch(`${API_BASE_URL}/projects`, {
+    cache: "no-store",
+  });
+  return projects.json();
 }
