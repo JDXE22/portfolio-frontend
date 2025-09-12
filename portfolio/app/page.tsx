a06b7b0fe6d7a7fa6aa5daf309a4491f7f444a66
David <davidesparzaj22@gmail.com>
2025-09-12 15:54:40 -0500

    feat(portfolio/app/page.tsx):
    add ContactSection to Home page

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,5 +1,6 @@
 import AboutSection from "@/app/(components)/sections/aboutSection/aboutSection";
 import ProjectsSection from "@/app/(components)/sections/projectsSection/projectsSection";
+import ContactSection from "@/app/(components)/sections/contactSection/contactSection";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       Home
       <AboutSection />
       <ProjectsSection />
+      <ContactSection />
     </div>
   );
 }
