f57cc5ed945345c4be9de3ddf857c0eea160241d
David <davidesparzaj22@gmail.com>
2025-09-15 19:18:05 -0500

    refactor(portfolio/app/(components)
    /layout/Section.tsx):
    
    simplify Section component props

--- portfolio/app/(components)/layout/Section.tsx
+++ portfolio/app/(components)/layout/Section.tsx
@@ -1,20 +1,13 @@
 import { SectionProps } from "@/types/types";
 
-export function Section({
-  id,
-  title,
-  subtitle,
-  className,
-  children,
-}: SectionProps) {
+export function Section({ id, title, className, children }: SectionProps) {
   return (
     <>
       <section id={id} className={`w-full px-6 py-16 sm:py-24 ${className}`}>
         <div className="mx-auto max-w-5xl">
           {title && (
             <header className="mb-8">
-              <h2 className="text-2xl sm:text-lg font-semibold">{title}</h2>
-              {subtitle && <p>{subtitle}</p>}
+              <h2 className="text-lg sm:text-2xl font-semibold">{title}</h2>
             </header>
           )}
           {children}
