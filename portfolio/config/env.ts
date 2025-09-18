893c8398995233af81e3fe35b8398ff3515f55b2
David <davidesparzaj22@gmail.com>
2025-09-18 18:52:29 -0500

    feat(portfolio/config/env.ts):
    add publicEnv for client-side DB
    
    connection variables

--- portfolio/config/env.ts
+++ portfolio/config/env.ts
@@ -1,3 +1,8 @@
 export const serverEnv = {
   PUBLIC_DB_CONNECTION: process.env.PUBLIC_DB_CONNECTION ?? ``,
 } as const;
+
+export const publicEnv = {
+  NEXT_PUBLIC_DB_CONNECTION:
+    process.env.NEXT_PUBLIC_DB_CONNECTION ?? process.env.PUBLIC_DB_CONNECTION,
+} as const;
