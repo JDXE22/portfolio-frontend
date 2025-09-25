e8acd3ca5b9ef80f6720027c32fbee0a416ce394
David <davidesparzaj22@gmail.com>
2025-09-25 13:10:42 -0500

    refactor(portfolio/app/[lang]/page.tsx):
    update Home page to handle lang param

--- portfolio/app/[lang]/page.tsx
+++ portfolio/app/[lang]/page.tsx
@@ -3,7 +3,10 @@ import ProjectsSection from "@/app/(components)/sections/projectsSection/project
 import ContactSection from "@/app/(components)/sections/contactSection/contactSection";
 import { HeroSection } from "@/app/(components)/sections/heroSection/heroSection";
 
-export default function Home() {
+export default async function Home(
+  props: Readonly<{ params: { lang: string } }>
+) {
+  const { lang } = await props.params;
   return (
     <main className="flex min-h-screen flex-col items-stretch">
       <HeroSection />
