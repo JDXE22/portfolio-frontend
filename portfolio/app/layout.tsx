5322ff831de191d1947d65c32b0c0a7444277af6
JDEX22 <davidesparzaj22@gmail.com>
2025-10-06 13:52:31 -0500

    fix(portfolio/app/[lang]/layout.tsx):
    resolve hydration suppression error

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -1,11 +1,32 @@
+import './globals.css';
+import { Geist, Geist_Mono } from 'next/font/google';
+import { Metadata } from 'next';
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Portfolio',
+  description: 'Created by David Esparza',
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html suppressHydrationWarning>
-      <body suppressHydrationWarning>{children}</body>
+    <html>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased app-gradient`}>
+        {children}
+      </body>
     </html>
   );
 }
