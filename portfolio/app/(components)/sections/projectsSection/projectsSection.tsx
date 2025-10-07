3b2e6c5af8b7c1fb35d0265a204c8c2db6bafa9b
JDEX22 <davidesparzaj22@gmail.com>
2025-10-07 18:25:06 -0500

    refactor(portfolio/app/(components)/
    sections/projectsSection/projectsSection.tsx):
    
    remove title prop from Section component

--- portfolio/app/(components)/sections/projectsSection/projectsSection.tsx
+++ portfolio/app/(components)/sections/projectsSection/projectsSection.tsx
@@ -1,19 +1,18 @@
-import { getProjects } from "@/data/dataApi";
-import { Section } from "@/app/(components)/layout/Section";
-import { IProject } from "@/types/types";
-import ProjectsGrid from "@/app/(components)/sections/projectsSection/projectsGrid";
+import { getProjects } from '@/data/dataApi';
+import { Section } from '@/app/(components)/layout/Section';
+import { IProject } from '@/types/types';
+import ProjectsGrid from '@/app/(components)/sections/projectsSection/projectsGrid';
 
 export default async function ProjectsSection() {
   const projects: IProject[] = await getProjects();
 
   return (
-    <Section id="projects" title="Projects" align="center" equalHeight>
-      <div className="h-full w-full flex items-center justify-center py-10">
+    <Section id='projects' align='center' equalHeight>
+      <div className='h-full w-full flex items-center justify-center py-10'>
         {projects.length === 0 ? (
           <p
-            data-testid="projects-empty"
-            className="m-auto text-center text-foreground/50"
-          >
+            data-testid='projects-empty'
+            className='m-auto text-center text-foreground/50'>
             []
           </p>
         ) : (
