2c0aef3f2184cc3bb7cd34076d8a0dea11c7f973
JDEX22 <davidesparzaj22@gmail.com>
2025-10-16 16:06:14 -0500

    refactor(portfolio/app/(components)/layout
    /SitePanel.tsx):
    
    Updated the SitePanel component's inner container
    
    max width from 170vh to 200vh to allow for a wider
    
    layout.

--- portfolio/app/(components)/layout/SitePanel.tsx
+++ portfolio/app/(components)/layout/SitePanel.tsx
@@ -7,7 +7,7 @@ type SitePanelProps = {
 export default function SitePanel({ children }: SitePanelProps) {
   return (
     <div className='w-full bg-background/90 h-full'>
-      <div className='mx-auto w-full max-w-[170vh] bg-background/60 backdrop-blur-sm rounded-2xl border-foreground/12 shadow-2xl overflow-visible'>
+      <div className='mx-auto w-full max-w-[200vh] bg-background/60 backdrop-blur-sm rounded-2xl border-foreground/12 shadow-2xl overflow-visible'>
         {children}
       </div>
     </div>
