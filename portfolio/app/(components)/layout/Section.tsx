7df6e11c536a75fe26ee693a3aa3ff4b5c08cbdc
JDEX22 <davidesparzaj22@gmail.com>
2025-10-16 12:57:43 -0500

    refactor(portfolio/app/(components)/layout
    /Section.tsx):
    
    Refactor Section component formatting and classes

--- portfolio/app/(components)/layout/Section.tsx
+++ portfolio/app/(components)/layout/Section.tsx
@@ -1,51 +1,48 @@
-import { SectionProps } from "@/types/types";
+import { SectionProps } from '@/types/types';
 
 export function Section({
   id,
   title,
   className,
   children,
-  align = "left",
+  align = 'left',
   noPadding,
   noMinHeight,
   equalHeight,
   customMinHeight,
 }: SectionProps) {
   const alignmentClasses = {
-    left: "text-left",
-    center: "text-center",
-    right: "text-right",
+    left: 'text-left',
+    center: 'text-center',
+    right: 'text-right',
   }[align];
 
-  const paddingClasses = noPadding ? "px-0 py-0" : "px-6 py-20 sm:py-28";
+  const paddingClasses = noPadding ? 'px-0 py-0' : 'px-6 py-20 sm:py-28';
   const heightClass = noMinHeight
-    ? ""
+    ? ''
     : customMinHeight
     ? customMinHeight
-    : "min-h-[560px]";
+    : 'min-h-[560px]';
 
-  const flexClass = equalHeight ? "flex-1" : "";
+  const flexClass = equalHeight ? 'flex-1' : '';
 
   return (
     <>
       <section
         id={id}
-        className={`w-full ${paddingClasses} ${className || ""}`}
-      >
+        className={`w-full ${paddingClasses} ${className || ''}`}>
         <div
-          className={`mx-auto max-w-5xl flex flex-col ${flexClass} ${heightClass}`}
-        >
+          className={`mx-auto max-w-full flex flex-col ${flexClass} ${heightClass}`}>
           {title && (
             <header className={`mb-10`}>
               <h2
-                className={`${alignmentClasses} text-2xl sm:text-3xl font-semibold tracking-tight`}
-              >
+                className={`${alignmentClasses} text-2xl sm:text-3xl font-semibold tracking-tight`}>
                 {title}
               </h2>
             </header>
           )}
 
-          <div className="flex-1">{children}</div>
+          <div className='flex-1'>{children}</div>
         </div>
       </section>
     </>
