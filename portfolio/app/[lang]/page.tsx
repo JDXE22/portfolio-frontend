6a250fe86ee55c9cf302925539bc29efd0af8f8f
JDEX22 <davidesparzaj22@gmail.com>
2025-10-16 12:33:11 -0500

    refactor(portfolio/app/[lang]/page.tsx):
    Wrap homepage sections with SitePanel layout

--- portfolio/app/[lang]/page.tsx
+++ portfolio/app/[lang]/page.tsx
@@ -1,17 +1,20 @@
-import AboutSection from "@/app/(components)/sections/aboutSection/aboutSection";
-import ProjectsSection from "@/app/(components)/sections/projectsSection/projectsSection";
-import ContactSection from "@/app/(components)/sections/contactSection/contactSection";
-import { HeroSection } from "@/app/(components)/sections/heroSection/heroSection";
-import TechnologiesSection from "@/app/(components)/sections/technologiesSection/techSection";
+import AboutSection from '@/app/(components)/sections/aboutSection/aboutSection';
+import ProjectsSection from '@/app/(components)/sections/projectsSection/projectsSection';
+import ContactSection from '@/app/(components)/sections/contactSection/contactSection';
+import { HeroSection } from '@/app/(components)/sections/heroSection/heroSection';
+import TechnologiesSection from '@/app/(components)/sections/technologiesSection/techSection';
+import SitePanel from '@/app/(components)/layout/SitePanel';
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-stretch">
-      <HeroSection />
-      <AboutSection />
-      <TechnologiesSection/>
-      <ProjectsSection />
-      <ContactSection />
+    <main className='flex min-h-screen flex-col items-stretch w-full h-full'>
+      <SitePanel>
+        <HeroSection />
+        <AboutSection />
+        <TechnologiesSection />
+        <ProjectsSection />
+        <ContactSection />
+      </SitePanel>
     </main>
   );
 }
